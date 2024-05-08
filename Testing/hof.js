7ffd6cd77d0c9fab30bbba9e8d6fe5cabe09ce15
amranhossain <amranhossainchy@gmail.com>
2024-05-08 23:30:31 +0600

    added

---
+++ Testing/hof.js
@@ -0,0 +1,18 @@
+
+
+console.log(sum1(10, 3))
+
+
+function sum1(n1, n2){
+    return n1 + n2
+}
+
+console.log(x)
+let x = function(){
+    return 10;
+};
+
+
+
+
+
---
+++ function/function.js
@@ -0,0 +1,7 @@
+/**
+ * There is three way to declare a function
+ *  1. Function Declaration
+ *  2. Function Expression
+ *  3. Named Function Expression
+ * 
+ * */
